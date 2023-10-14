3c8f76c84d32587f0f597e25fb1e2fbc8906c5ca
Dongho Jung <bnmjko2000@snu.ac.kr>
2023-10-14 15:47:27 +0900

    feat: implement state restoring(student, professor)

--- app/src/main/java/com/example/goclass/MainActivity.kt
+++ app/src/main/java/com/example/goclass/MainActivity.kt
@@ -1,29 +1,57 @@
 package com.example.goclass
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.goclass.view.LoginFragment
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val profileButton: Button = findViewById(R.id.profileButton)
-        val class1Button: Button = findViewById(R.id.class1Button)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-        profileButton.setOnClickListener {
-            val intent = Intent(this, ProfileActivity::class.java)
-            startActivity(intent)
-        }
+        // Check if logged in
+        checkLoginStatus()
+    }
 
-        class1Button.setOnClickListener {
-            val intent = Intent(this, ChatActivity::class.java)
-            startActivity(intent)
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun checkLoginStatus() {
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val isLoggedIn = sharedPref.getBoolean("isLoggedIn", false)
+        val userRole = sharedPref.getString("userRole", "")
+
+        if (isLoggedIn) {
+            when (userRole) {
+                "student" -> {
+                    navController.navigate(R.id.studentMainFragment)
+                }
+                "professor" -> {
+                    navController.navigate(R.id.professorMainFragment)
+                }
+                else -> {
+                    navController.navigate(R.id.profileFragment)
+                }
+            }
+        } else {
+            navController.navigate(R.id.loginFragment)
         }
     }
 }
---
+++ app/src/main/java/com/example/goclass/view/ProfileFragment.kt
@@ -0,0 +1,101 @@
+package com.example.goclass.view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.goclass.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        val errorTextView = view.findViewById<TextView>(R.id.errorTextView)
+
+        // Logout Button
+        view.findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            sharedPref ?: return@setOnClickListener
+            with(sharedPref.edit()) {
+                putBoolean("isLoggedIn", false)
+                apply()
+            }
+            with(sharedPref.edit()) {
+                putString("userRole", "")
+                apply()
+            }
+            findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+        }
+
+        // Confirm Button
+        val radioGroup = view.findViewById<RadioGroup>(R.id.roleRadioGroup)
+        val confirmButton = view.findViewById<Button>(R.id.confirmButton)
+
+        confirmButton.setOnClickListener {
+            val selectedRole = when (radioGroup.checkedRadioButtonId) {
+                R.id.studentRadioButton -> "student"
+                R.id.professorRadioButton -> "professor"
+                else -> null
+            }
+
+            if (selectedRole == null) {
+                errorTextView.visibility = View.VISIBLE
+            }
+            else {
+                errorTextView.visibility = View.GONE
+            }
+
+            selectedRole?.let {
+                val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+                with(sharedPref?.edit()) {
+                    this?.putString("userRole", it)
+                    this?.apply()
+                }
+
+                when (it) {
+                    "student" -> {
+                        findNavController().navigate(R.id.action_profileFragment_to_studentMainFragment)
+                    }
+                    "professor" -> {
+                        findNavController().navigate(R.id.action_profileFragment_to_professorMainFragment)
+                    }
+                }
+            }
+        }
+
+        // Remember whether user is a student or a professor
+        when (sharedPref?.getString("userRole", "")) {
+            "student" -> {
+                view.findViewById<RadioButton>(R.id.studentRadioButton).isChecked = true
+            }
+            "professor" -> {
+                view.findViewById<RadioButton>(R.id.professorRadioButton).isChecked = true
+            }
+            else -> {
+                // Do Nothing
+            }
+        }
+
+        // Keyboard down when you touch other space in screen
+        view.setOnTouchListener { _, _ ->
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
+            false
+        }
+
+    }
+}
