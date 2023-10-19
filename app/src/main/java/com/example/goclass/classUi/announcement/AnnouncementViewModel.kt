b8bdea759b0eb35bd4a9e0215ec6e33e7683564a
Dongmin Lee <ldm9100@snu.ac.kr>
2023-10-19 23:34:25 +0900

    style: fit to lint

--- app/src/main/java/com/example/goclass/classUi/announcement/AnnouncementViewModel.kt
+++ app/src/main/java/com/example/goclass/classUi/announcement/AnnouncementViewModel.kt
@@ -5,9 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AnnouncementViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>()
-        .apply {
+    private val _text = MutableLiveData<String>().apply {
             value = "This is Announcement Channel"
-        }
+    }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/goclass/classUi/qna/QnaViewModel.kt
+++ app/src/main/java/com/example/goclass/classUi/qna/QnaViewModel.kt
@@ -5,9 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class QnaViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>()
-        .apply {
+    private val _text = MutableLiveData<String>().apply {
             value = "This is Q&A Channel"
-        }
+    }
     val text: LiveData<String> = _text
 }
