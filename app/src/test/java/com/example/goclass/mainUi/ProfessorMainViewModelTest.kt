548edfb2160733d8beaa62845ebb96f9eb196813
woogeun jeong <bobby092@naver.com>
2023-11-02 17:43:57 +0900

    test: implemented mokito

---
+++ app/src/test/java/com/example/goclass/mainUi/ProfessorMainViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.example.goclass.mainUi
+
+import org.junit.jupiter.api.Assertions.*
+
+class ProfessorMainViewModelTest {
+
+    @org.junit.jupiter.api.Test
+    fun getToastMessage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getClassListLiveData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun createClass() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getClassList() {
+    }
+}
