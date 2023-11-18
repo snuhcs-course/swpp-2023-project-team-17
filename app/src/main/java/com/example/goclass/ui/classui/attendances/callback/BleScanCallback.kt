d9c01e85f48561ff8d7e035a5f5c43dbc8899f47
woogeun jeong <bobby092@naver.com>
2023-11-18 20:36:52 +0900

    style: fit to lint

--- app/src/main/java/com/example/goclass/ui/classui/attendances/callback/BleScanCallback.kt
+++ app/src/main/java/com/example/goclass/ui/classui/attendances/callback/BleScanCallback.kt
@@ -2,6 +2,8 @@ package com.example.goclass.ui.classui.attendances.callback
 
 interface BleScanCallback {
     fun onDeviceFound(scanCount: Int)
+
     fun onScanFailed(errorCode: Int)
+
     fun onScanFinish()
 }
--- app/src/main/java/com/example/goclass/ui/classui/attendances/callback/LocationCallback.kt
+++ app/src/main/java/com/example/goclass/ui/classui/attendances/callback/LocationCallback.kt
@@ -2,5 +2,4 @@ package com.example.goclass.ui.classui.attendances.callback
 
 interface LocationCallback {
     fun onLocationStatus(isInLocation: Boolean)
-
 }
