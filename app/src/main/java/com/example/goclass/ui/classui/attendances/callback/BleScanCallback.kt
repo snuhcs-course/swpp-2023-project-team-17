bd7e72eeede216589aed6d68f6b56623094f2068
woogeun jeong <69378301+jwg1108@users.noreply.github.com>
2023-12-04 10:29:14 +0900

    Revert "Feat/attendance detail"

--- app/src/main/java/com/example/goclass/ui/classui/attendances/callback/BleScanCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.example.goclass.ui.classui.attendances.callback
-
-interface BleScanCallback {
-    fun onDeviceFound(scanCount: Int)
-
-    fun onScanFailed(errorCode: Int)
-
-    fun onScanFinish()
-}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        maven {
-            url = uri("https://plugins.gradle.org/m2/")
-        }
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15")
-        classpath("io.github.gmazzo.test.aggregation:plugin:2.1.1")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
-    }
-}
-
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
 }
-
-apply(plugin = "io.github.gmazzo.test.aggregation.coverage")
